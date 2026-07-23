33593bc8302e58481847f92c00582f5f87aeaab2
datvct <vancongthanhdata10@gmail.com>
2026-07-23 10:12:06 +0700

    fix: update

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,6 +1,6 @@
 // CONFIG.JS
 // Nơi DUY NHẤT cần sửa sau khi deploy Apps Script Web App.
 // Dán URL kết thúc bằng /exec, KHÔNG dán URL có /dev.
-// const API_URL = 'https://script.google.com/macros/s/AKfycbyhLKa-70_r44ROUUE1hgg13_dfw4NkYTjoXzsgSjS-uA6KoTAM5kO_44jr3NoAYHV9/exec';
+const API_URL = 'https://script.google.com/macros/s/AKfycbyhLKa-70_r44ROUUE1hgg13_dfw4NkYTjoXzsgSjS-uA6KoTAM5kO_44jr3NoAYHV9/exec';
 
-const API_URL = 'https://script.google.com/macros/s/AKfycbyhLKa-70_r44ROUUE1hgg13_dfw4NkYTjoXzsgSjS-uA6KoTAM5kO_44jr3NoAYHV9/exec'
+// const API_URL = 'https://script.google.com/macros/s/AKfycbx-brdl1U_THYc29ZM_fFiM2t6GgduNJWz0cVhg5nnJOuizA-YpUuvEKI6ZxzY90D3b/exec'
